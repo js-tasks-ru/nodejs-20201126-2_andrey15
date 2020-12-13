a3c6d579df66e030d803950571d3ee1be05ba8c2
a.m.korolev <a.m.korolev@tinkoff.ru>
2020-12-13 15:42:36 +0300

    add solution for 6-koajs 1-chat-app

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,42 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const resolveFunctionList = new Map();
+
 router.get('/subscribe', async (ctx, next) => {
+  const {r} = ctx.request.query;
+
+  const promise = new Promise((resolve, reject) => {
+    resolveFunctionList.set(r, resolve);
+
+    ctx.req.on('close', () => {
+      if (resolveFunctionList.has(r)) {
+        resolveFunctionList.delete(r);
+      }
+    });
+  });
+
+  await promise;
+
+  promise.then((data) => {
+    ctx.body = data;
+  });
+
+  return next();
 });
 
 router.post('/publish', async (ctx, next) => {
+  const {message} = ctx.request.body;
+
+  resolveFunctionList.forEach((value, valueAgain, set) => {
+    value(message);
+  });
+
+  resolveFunctionList.clear();
+
+  ctx.status = 200;
+
+  return next();
 });
 
 app.use(router.routes());
